ed3d0ad1b024bb0e8fa3625d954a176103458a64
Angel Dominguez <angeldomp49@gmail.com>
2022-04-06 11:50:03 -0500

    DiscountFactor

--- src/main/java/org/makechtec/financial/financialutils/factor/DiscountFactor.java
+++ src/main/java/org/makechtec/financial/financialutils/factor/DiscountFactor.java
@@ -1,5 +1,35 @@
 package org.makechtec.financial.financialutils.factor;
 
+import org.makechtec.financial.financialutils.rate.CompoundRate;
+import org.makechtec.financial.financialutils.rate.Rate;
+
 public class DiscountFactor {
-    
+
+    private double value;
+    private Rate growth;
+    private CompoundRate compoundRate;
+
+    public DiscountFactor(CompoundRate compoundRate){
+        this.compoundRate = compoundRate;
+        this.generateValue();
+    }
+
+    public DiscountFactor(CompoundRate compoundRate, Rate growth){
+        this.compoundRate = compoundRate;
+        this.growth = growth;
+        this.generateValue(growth);
+    }
+
+    public void generateValue(){
+        double dividend = 1 - (1 / this.compoundRate.getValue());
+        this.value = dividend/this.compoundRate.getRate().getValue();
+    }
+
+    public void generateValue(Rate growth){
+        SimpleFactor rateFactor = new SimpleFactor(this.compoundRate);
+        SimpleFactor growthFactor = new SimpleFactor(new CompoundRate(growth, this.compoundRate.getPeriod()));
+        double ratio = growthFactor.getValue()/rateFactor.getValue();
+        this.value = (1- ratio)/(this.compoundRate.getRate().getValue() - growth.getValue());
+    }
+
 }
