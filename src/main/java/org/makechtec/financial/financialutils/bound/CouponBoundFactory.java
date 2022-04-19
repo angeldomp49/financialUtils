bf3da67d3da25201daa71bdc6b3118b76f4c6cdc
Angel Dominguez <angeldomp49@gmail.com>
2022-04-19 00:35:24 -0500

    zero coupon bound

--- src/main/java/org/makechtec/financial/financialutils/bound/CouponBoundFactory.java
+++ src/main/java/org/makechtec/financial/financialutils/bound/CouponBoundFactory.java
@@ -1,12 +1,22 @@
 package org.makechtec.financial.financialutils.bound;
 
+import org.makechtec.financial.financialutils.cashflow.SimpleCashFlow;
 import org.makechtec.financial.financialutils.cashflow.SimpleCashFlowFactory;
+import org.makechtec.financial.financialutils.cashflow.SimpleCashFlowFactory.CashFlowRate;
+import org.makechtec.financial.financialutils.rate.CompoundRate;
 
 public class CouponBoundFactory {
 
     private SimpleCashFlowFactory simpleCashFlowFactory = new SimpleCashFlowFactory();
+    private CashFlowRate cashFlowRate = new CashFlowRate();
 
-    public ZeroCouponBound create( double initialAmount, double finalAmount, long period){
-        return new ZeroCouponBound(this.simpleCashFlowFactory.create(initialAmount, finalAmount, period));
+    // public ZeroCouponBound create( double initialAmount, double finalAmount, long period){
+    //     return new ZeroCouponBound(this.simpleCashFlowFactory.create(initialAmount, finalAmount, period));
+    // }
+
+    public ZeroCouponBound create(double faceValue, double discountRate, long period){
+        CompoundRate compoundRate = this.cashFlowRate.create(discountRate, period);
+        SimpleCashFlow cashFlow = this.simpleCashFlowFactory.create(compoundRate, faceValue);
+        return new ZeroCouponBound(cashFlow);
     }
 }
